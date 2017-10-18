9f57e39919cbcb2797febb7225e7f56e13eab832
BenjiFischman <benjiricefischman@gmail.com>
2017-10-18 12:19:36 -0500

    skelaton  header with nav and search

--- public/javascripts/local.js
+++ public/javascripts/local.js
@@ -1,3 +1,18 @@
 $(function() {
   console.log('local.js ready');
 });
+
+
+/*$(function() {
+    $('.toggle-nav').click(function(e) {
+        $(this).toggleClass('active');
+        $('.menu ul').toggleClass('active');
+ 
+        e.preventDefault();
+    });
+});
+*/
+function myFunction(x) {
+    x.classList.toggle("change");
+    $(".menulist").toggle();
+}
